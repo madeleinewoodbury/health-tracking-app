0ab8c2ddbc4224248914c623720b7874dee641cb
Madeleine Woodbury <mail@woodburycode.com>
2024-12-01 14:05:03 -0500

    refactor data analysis to be handled by node api and database queries

--- node-api/src/handlers/admin.ts
+++ node-api/src/handlers/admin.ts
@@ -1,44 +1,31 @@
 import prisma from '../db'
 
 /**
- * Handles the request to get user activity per date.
+ * Retrieves the number of unique users' activity per day from the database.
  *
- * This function fetches activity logs from the database and sends them to an external
- * service for further processing. The response from the external service is then
- * returned to the client.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @returns A JSON response containing an array of objects with the date and the count of unique users for each day.
  */
 export const getUserActivtyPerDay = async (req, res) => {
 	try {
-		// Fetch the activities from the database
-		const activities = await prisma.activityLog.findMany({
-			select: {
-				userId: true,
-				method: true,
-				endpoint: true,
-				recordedAt: true,
-			},
-		})
+		// Fetch activity logs from the database and group them by date and count the number of unique users
+		const result: { date: string; uniqueUsers: bigint }[] =
+			await prisma.$queryRaw`
+			SELECT 
+				DATE("recordedAt") as date, 
+				COUNT(DISTINCT "userId") as "uniqueUsers"
+			FROM "ActivityLog"
+			GROUP BY DATE("recordedAt")
+			ORDER BY DATE("recordedAt")
+		`
 
-		if (!activities) {
-			throw new Error('No activities found')
-		}
-
-		const response = await fetch(
-			'http://127.0.0.1:8000/activity-tracking/users',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(activities),
-			}
-		)
-
-		if (!response.ok) {
-			throw new Error('Failed to fetch data')
-		}
-
-		const data = await response.json()
+		// Format the date and convert the uniqueUsers count to a number
+		const data = result.map((row) => ({
+			date: new Date(row.date).toISOString().split('T')[0],
+			// Convert BigInt to Number
+			uniqueUsers: Number(row.uniqueUsers),
+		}))
 
 		res.json(data)
 	} catch (error) {
@@ -48,23 +35,119 @@ export const getUserActivtyPerDay = async (req, res) => {
 }
 
 /**
- * Handles the request to get trends in symptom reporting across different locations.
+ * Retrieves symptom counts by location from the database.
+ *
+ * @param req - The request object containing the query with `country`, `city`, and `state`.
+ * @param res - The response object used to send back the symptom counts or an error message.
+ *
+ * @returns A JSON response containing the location details and aggregated symptom counts.
  */
-export const getGeographicTrends = async (req, res) => {
+export const getSymptomCountsByLocation = async (req, res) => {
 	try {
-		// Get symptom logs from the database
-		const queryResults = await prisma.userSymptomLog.findMany({
+		const { country, city, state } = req.query
+
+		// Get location data from the database
+		const location = await prisma.location.findFirst({
+			where: {
+				city,
+				state,
+				country: {
+					name: country,
+				},
+			},
 			select: {
-				location: {
+				id: true,
+				city: true,
+				state: true,
+				country: {
 					select: {
-						country: {
-							select: {
-								name: true,
-							},
-						},
-						city: true,
+						name: true,
 					},
 				},
+			},
+		})
+
+		// If location is not found, return an error
+		if (!location) {
+			return res.status(404).json({ error: 'Location not found' })
+		}
+
+		// Get aggregated symptom counts for the location
+		const symptoms = await prisma.userSymptomEntry.groupBy({
+			by: ['symptomId'],
+			_count: {
+				symptomId: true,
+			},
+			where: {
+				log: {
+					locationId: location.id,
+				},
+			},
+		})
+
+		// Join symptom names with their counts
+		const symptomDetails = await Promise.all(
+			// Map over each symptom and get the name
+			symptoms.map(async (symptom) => {
+				const symptomInfo = await prisma.symptom.findUnique({
+					where: { id: symptom.symptomId },
+					select: { name: true },
+				})
+
+				return {
+					symptom: symptomInfo.name,
+					count: symptom._count.symptomId,
+				}
+			})
+		)
+
+		// Construct the response
+		const response = {
+			country: location.country.name,
+			city: location.city,
+			state: location.state || null,
+			symptoms: symptomDetails,
+		}
+
+		res.json(response)
+	} catch (error) {
+		console.error('Failed to fetch symptom counts:', error)
+		res.status(500).json({ error: 'Internal Server Error' })
+	}
+}
+
+/**
+ * Retrieves common symptom patterns associated with a given symptom.
+ *
+ * @param req - The request object containing the symptom name in the query.
+ * @param res - The response object used to send the result or an error message.
+ *
+ * @returns A JSON response with an array of common symptoms and their counts, or an error message.
+ */
+export const getSymptomPatterns = async (req, res) => {
+	try {
+		const { symptom } = req.query
+
+		// Find the symptom ID for the given symptom name
+		const symptomRecord = await prisma.symptom.findUnique({
+			where: { name: symptom },
+			select: { id: true },
+		})
+
+		if (!symptomRecord) {
+			return res.status(404).json({ error: 'Symptom not found' })
+		}
+
+		// Get logs that include the given symptom
+		const symptomLogs = await prisma.userSymptomLog.findMany({
+			where: {
+				userSymptomEntries: {
+					some: {
+						symptomId: symptomRecord.id,
+					},
+				},
+			},
+			select: {
 				userSymptomEntries: {
 					select: {
 						symptom: {
@@ -74,44 +157,28 @@ export const getGeographicTrends = async (req, res) => {
 						},
 					},
 				},
-				recordedAt: true,
 			},
 		})
 
-		if (!queryResults) {
-			throw new Error('No symptom logs found')
-		}
+		// Aggregate and count occurrences of other symptoms
+		const symptomCounts = symptomLogs.reduce((acc, log) => {
+			log.userSymptomEntries.forEach((entry) => {
+				if (entry.symptom.name !== symptom) {
+					// Increment the count for the symptom name
+					acc[entry.symptom.name] = (acc[entry.symptom.name] || 0) + 1
+				}
+			})
+			return acc
+		}, {})
 
-		// Transform the data and send it to an external service
-		const symptomLogs = queryResults.map((log) => {
-			return {
-				country: log.location.country.name,
-				city: log.location.city,
-				symptoms: log.userSymptomEntries.map((entry) => entry.symptom.name),
-				recordedAt: log.recordedAt,
-			}
-		})
+		// Convert the counts object to an array and sort by count
+		const commonSymptoms = Object.entries(symptomCounts)
+			.map(([name, count]) => ({ name, count }))
+			.sort((a, b) => Number(b.count) - Number(a.count))
 
-		console.log('Sending data to external service')
-		const response = await fetch(
-			'http://127.0.0.1:8000/activity-tracking/geography',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(symptomLogs),
-			}
-		)
-
-		if (!response.ok) {
-			throw new Error('Failed to fetch data')
-		}
-
-		const data = await response.json()
-		res.json(data)
+		res.json(commonSymptoms)
 	} catch (error) {
-		console.error('Failed to get geographic trends:', error)
+		console.error('Failed to get symptom patterns:', error)
 		res.status(500).json({ error: 'Internal Server Error' })
 	}
 }
--- node-api/src/routes/admin.ts
+++ node-api/src/routes/admin.ts
@@ -1,10 +1,49 @@
 import { Router } from 'express'
-import { getUserActivtyPerDay, getGeographicTrends } from '../handlers/admin'
+import { query } from 'express-validator'
+import { handleInputErrors } from '../modules/middleware'
+import {
+	getUserActivtyPerDay,
+	getSymptomCountsByLocation,
+	getSymptomPatterns,
+} from '../handlers/admin'
 
 const router = Router()
 
+/*
+ * GET /admin/activity
+ * Access: Admin
+ * Description: Returns the number of unique user per day.
+ */
 router.get('/activity', getUserActivtyPerDay)
 
-router.get('/geographic', getGeographicTrends)
+/*
+ * GET /admin/geographic
+ * Access: Admin
+ * Query: { country: string, city: string, state?: string }
+ * Description: Returns the symptom counts for the given location.
+ */
+router.get(
+	'/geographic',
+	[
+		query('country').isString(),
+		query('city').isString(),
+		query('state').isString().optional(),
+	],
+	handleInputErrors,
+	getSymptomCountsByLocation
+)
+
+/*
+ * GET /admin/symptoms
+ * Access: Admin
+ * Query: { symptom: string }
+ * Description: Returns the patterns of the given symptom.
+ */
+router.get(
+	'/symptoms',
+	query('symptom').isString(),
+	handleInputErrors,
+	getSymptomPatterns
+)
 
 export default router
